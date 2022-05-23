1e68f41025ef680eb9cd9f969c75fb611e907cf6
Craig Govender <craig.govender@rank.com>
2022-05-23 13:43:20 +0200

    Lint fixes

--- src/app/modules/google-sign-in/google-sign-in.component.ts
+++ src/app/modules/google-sign-in/google-sign-in.component.ts
@@ -4,6 +4,10 @@ import { GoogleSignInSettings } from './google-sign-in.settings';
 import { Subject } from 'rxjs';
 import { DOCUMENT } from '@angular/common';
 
+/**
+ * This is done so that the 'onGoogleSignIn' method can be added to the window object
+ * @see Document.defaultView
+ */
 type CustomGoogleWindow = WindowProxy & typeof globalThis & {onGoogleSignIn?: (resp: GoogleSignInResponse) => void};
 
 @Component({
@@ -12,11 +16,11 @@ type CustomGoogleWindow = WindowProxy & typeof globalThis & {onGoogleSignIn?: (r
 })
 export class GoogleSignInComponent {
 
-  public callbackMethodName = GoogleSignInSettings.CallbackMethodName;
-  public clientID = GoogleSignInSettings.ClientID;
-  public readonly credentials$ = new Subject<GoogleSignInResponse>()
+  public readonly callbackMethodName = GoogleSignInSettings.CallbackMethodName;
+  public readonly clientID = GoogleSignInSettings.ClientID;
+  public readonly credentials$ = new Subject<GoogleSignInResponse>();
 
-  private readonly customEventName = GoogleSignInSettings.CustomEventName
+  private readonly customEventName = GoogleSignInSettings.CustomEventName;
   private readonly window: CustomGoogleWindow | null;
 
   @HostListener('window:onGoogleSignInEvent', ['$event'])
