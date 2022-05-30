b645d2ccc0346b89f14d369472270548b06b8cde
Craig Govender <craig.govender@rank.com>
2022-05-30 09:56:19 +0200

    Prevent multiple imports

--- src/app/modules/google-sign-in/google-sign-in.module.ts
+++ src/app/modules/google-sign-in/google-sign-in.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoogleSignInComponent } from './google-sign-in.component';
 import { GoogleSignInSettings } from './google-sign-in.settings';
@@ -16,6 +16,14 @@ import { GoogleSignInService } from './google-sign-in.service';
   ]
 })
 export class GoogleSignInModule {
+
+  constructor(@Optional() @SkipSelf() parentModule?: GoogleSignInModule) {
+    if (parentModule) {
+      throw new Error(
+        'GoogleSignInModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
   static forRoot(config: GoogleSignInSettings): ModuleWithProviders<GoogleSignInModule> {
     return {
       ngModule: GoogleSignInModule,
